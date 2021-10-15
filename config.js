3a7d960799c8feccb2a69697378bd355c463ac26
Stuart <sdeske@googlemail.com>
2021-10-15 17:40:29 +0100

    All missing files

---
+++ config.js
@@ -0,0 +1,9 @@
+// Local Hardhat
+//export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+//export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+
+// Mumbai Matic Polygon
+//nftMarket deployed to: 0x7e5669C44be3B6Ad75A0a121A18473915eB286FE
+//nft deployed to: 0x1d36D50a4c115fbb7e7640cDc7d7e1720c1Bc7Ca
+export const nftmarketaddress = "0x7e5669C44be3B6Ad75A0a121A18473915eB286FE"
+export const nftaddress = "0x1d36D50a4c115fbb7e7640cDc7d7e1720c1Bc7Ca"
